7883518f64f285f1b0381d9d1441ca080c479c92
Stefan Fredriksson <sf222pv@student.lnu.se>
2019-12-08 12:05:12 +0100

    Fixing bug

--- assignment3/Server/src/SearchEngine/Score.java
+++
@@ -1,9 +0,0 @@
-package SearchEngine;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Score {
-	public List<Double> content = new ArrayList<Double>();
-	public List<Double> location = new ArrayList<Double>();
-}
